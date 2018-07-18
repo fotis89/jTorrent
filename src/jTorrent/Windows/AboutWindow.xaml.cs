09dfec7c25dac33a9992f85acb8a19ed9d6dd71d
Fotis Karagiorgos <fotiskaragiorgos@gmail.com>
2018-07-19 01:16:28 +0300

    Add visible publish version

--- src/jTorrent/Windows/AboutWindow.xaml.cs
+++ src/jTorrent/Windows/AboutWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using System.Deployment.Application;
 using System.Diagnostics;
+using System.Reflection;
 using System.Windows;
-using jTorrent.ViewModels;
 
 namespace jTorrent.Windows
 {
@@ -13,10 +13,15 @@ namespace jTorrent.Windows
 		public AboutWindow()
 		{
 			Owner = Application.Current.MainWindow;
+
+			Version += ApplicationDeployment.IsNetworkDeployed
+				? ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString(4)
+				: Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
 			InitializeComponent();
 		}
 
 		public string AppUrl { get; set; } = "https://github.com/fotis89/jTorrent";
+		public string Version { get; } = "v";
 
 		private void Hyperlink_Click(object sender, RoutedEventArgs e)
 		{
